f85a2138b25b6b526e921f633a71ef4ceeba031d
Manav <manavchhabraj7@gmail.com>
2020-07-08 18:10:40 +0530

    Changing the contents to have the component sheet ready

---
+++ front-end/src/components/elements/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+    return <div className="header fixed-top"><h1>Component Sheet</h1></div>;
+}
+
+export default Header;
---
+++ front-end/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './css/styles.css'
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
