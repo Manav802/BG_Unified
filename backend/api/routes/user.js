98a69e6ebea26221a3f116d2cfdf5e56618ab0f8
vishuchhabra <vishuchhabra1016@gmail.com>
2020-07-08 22:40:29 +0530

    In progress

--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const Router = express.Router()
+const {sendSMS} = require('../../helper/sms')
 
 //Testing API
 
 
 Router.get('/user',(req, res)=>{
-    console.log("present ghe")
+  
     res.send("we are currently working for user")
- 
 })
 
 module.exports = Router
