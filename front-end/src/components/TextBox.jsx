1f1eb4684467cde01d79d4c7f00ae9cd62ed48f5
keysanyonthanm <keysangkeysang@gmail.com>
2020-07-11 15:06:56 +0530

    error textbox pushed, bug fixed

--- front-end/src/components/TextBox.jsx
+++ front-end/src/components/TextBox.jsx
@@ -3,15 +3,17 @@ import { Input, Stack } from "@chakra-ui/core";
 
 function Text(){
   return(
-    <form>
-      <Stack spacing="3">
-      <Input placeholder="Basic usage" />
-      <Input placeholder="Basic usage" />
-      <Input placeholder="Basic usage" />
-      <Input pr="4.5rem" type="password" placeholder="Enter password"
-      />
-      </Stack>
-    </form>
+    <div className="container">
+      <div className="row row-header">
+        <form>
+          <Stack spacing="3">
+          <Input placeholder="Username"/>
+          <Input pr="4.5rem" type="password" placeholder="Enter password"/>
+          <Input isInvalid errorBorderColor="red.300" placeholder="Wrong Password entered" />
+          </Stack>
+        </form>
+      </div>
+    </div>
   )
 }
 
