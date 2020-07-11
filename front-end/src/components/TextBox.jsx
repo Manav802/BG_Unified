63032a14e33ed730b320c58981c07353456d4785
keysanyonthanm <keysangkeysang@gmail.com>
2020-07-11 15:12:03 +0530

    textbox with focus pushed

--- front-end/src/components/TextBox.jsx
+++ front-end/src/components/TextBox.jsx
@@ -10,6 +10,8 @@ function Text(){
           <Input placeholder="Username"/>
           <Input pr="4.5rem" type="password" placeholder="Enter password"/>
           <Input isInvalid errorBorderColor="red.300" placeholder="Wrong Password entered" />
+          <Input focusBorderColor="lime" placeholder="textbox-success" />
+          <Input focusBorderColor="pink.400" placeholder="textbox-danger UNAGI!" />
           </Stack>
         </form>
       </div>
