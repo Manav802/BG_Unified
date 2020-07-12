c1149c08f17e84ab5cc052391abf7cff967d4e21
Yuvraj Midha <inventive8983@gmail.com>
2020-07-12 11:35:44 +0530

    commit

--- front-end/src/componentsheet/Componentsheet.jsx
+++ front-end/src/componentsheet/Componentsheet.jsx
@@ -5,11 +5,10 @@ import '../scss/_componentsheet.scss'
 
 //Components 
 import Login from '../components/auth/main';
-import CardWithIcon from '../components/cards/CardWithIcon'
-import CardWithAction from '../components/cards/CardWithAction'
-import CardWithImage from '../components/cards/CardWithImage'
-import CaseStudy from '../components/case_study/CaseStudy'
-import { FaBeer } from 'react-icons/fa';
+import CardWihAction from '../components/cards/CardWithAction'
+import CardWihImage from '../components/cards/CardWithImage'
+import CardWihIcon from '../components/cards/CardWithAction'
+
 
 // eslint-disableY
 
@@ -53,7 +52,7 @@ class Componentsheet extends Component {
                     <div className="col">
                         <div className="container d-flex justify-content-center">
                             <div className="row flex-fill m-0">
-                                <div className="col p-4 flex-column d-flex">
+                                <div className="col-lg-8 offset-2 p-4 flex-column d-flex">
                                     <span className="display3 mb-3">Components</span>
                                     <div className="my-3">
                                         <span className="display6">Authenticate</span>
@@ -63,27 +62,6 @@ class Componentsheet extends Component {
                                                 <span> </span>
                                                 <Login signup={true}/>
                                             </ButtonGroup>
-                                        </div>
-                                    </div>
-                                    <div className="my-3 row">
-                                        <span className="display6 col-12">Cards</span>
-                                        <div className="mt-2 col-lg-6">
-                                            <CardWithIcon className="col-lg-6" icon={FaBeer} title="Title Here">
-                                                Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr.
-                                            </CardWithIcon>
-                                        </div>
-                                        <div className="mt-2 col-lg-6">
-                                            <CardWithAction className="col-lg-6" icon={FaBeer} iconColor="Red" title="Title Here" action1="Action" action2="Action">
-                                                Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr.
-                                            </CardWithAction>
-                                        </div>
-                                        <div className="mt-2 col-12">
-                                            <CardWithImage className="col-lg-6" src="https://image.freepik.com/free-vector/colorful-abstract-wallpaper-design_23-2148467625.jpg" title="Title Here" action1="Action" action2="Action">
-                                                Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr.
-                                            </CardWithImage>
-                                        </div>
-                                        <div className="mt-2 col-12">
-                                            <CaseStudy></CaseStudy>
                                         </div>
                                     </div>
                                 </div>
