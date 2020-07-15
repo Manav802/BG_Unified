d3f49bc0d0bd2a4166ee67c12e0d9687cf31709d
Vineet Yadav <vineetyadav8588@gmail.com>
2020-07-16 00:01:21 +0530

    minor changes to cardwithicon

--- front-end/src/components/cards/CardWithIcon.jsx
+++ front-end/src/components/cards/CardWithIcon.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Box,Icon } from "@chakra-ui/core";
 
+//props.icon is an element (can be image,icon etc.)
 
 function CardWithIcon(props) {
     return (
         <div className="card-with-icon rounded p-3">
                 <div className="row iconbox">
                         <Box className="text-white" rounded="12px" w='48px' h='48px' padding='12px' bg={props.iconBg|| "Blue"}> 
-                            <Box as={props.icon} size="24px" color="white" />
+                            <Box size="24px" color="white" >
+                                {props.icon}
+                            </Box>
                         </Box>
                 </div>
                 <div className="row display6 mx-3">
