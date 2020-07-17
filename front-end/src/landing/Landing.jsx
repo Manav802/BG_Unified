5fccc08d5305240f2f4c8038b2a87881ce4d7235
Vineet Yadav <vineetyadav8588@gmail.com>
2020-07-17 17:07:25 +0530

    fix features svg

--- front-end/src/landing/Landing.jsx
+++ front-end/src/landing/Landing.jsx
@@ -19,14 +19,13 @@ import Icon10 from './Icons 2/Mask Group 10.svg'
 import Icon11 from './Icons 2/Mask Group 11.svg'
 import Icon12 from './Icons 2/Mask Group 12.svg'
 import Icon13 from './Icons 2/Mask Group 13.svg'
-import Icon14 from './Icons 2/Mask Group 14.svg'
+import Icon14 from './Icons 2/Mask Group 14.svg';
 
-import bgFeatures from '../static/images/backgrouds/bgFeatures.svg';
 import icNeon from '../static/images/backgrouds/ic_chip_neon.png';
-
 
 import {TESTIMONIALS} from '../shared/testimonials.jsx';
 import * as brands from './BrandIcons/index';
+
 
 function MyCard(props){
     return (
@@ -44,12 +43,16 @@ function MyCard(props){
     )
 }
 
-export class Landing extends Component {
+class Landing extends Component {
     render() {
+        var svgWidth=1920;
+        if(window.innerWidth>1920){
+            svgWidth=window.innerWidth;
+        }
         return (
             <div className="landing">
-                <div className="container">
 
+                <div className="container">
                     <div className="row first my-5">
                         <div className="col-12 col-lg-6 py-5">
                             <div className="welcome">
@@ -119,65 +122,66 @@ export class Landing extends Component {
                             </div>
                         </div>
                     </div>
-                    </div>
-                    <div className="row flex forth mt-5 py-lg-5 justify-content-center">
-                        <div className="position-absolute overflow-hidden w-100" >
-                        <svg style={{width:"1920px"}} height="864.684" viewBox="0 0 1922 864.684">
-                            <defs>
-                                <clipPath id="clip-path">
-                                <path id="Path_266" data-name="Path 266" d="M-318.159-17.14l1920-91.457V715.5l-1920-184.812Z" transform="translate(318.159 2059)" fill="#051133"/>
-                                </clipPath>
-                            </defs>
-                            <g id="Group_233" data-name="Group 233" transform="translate(0 -1950.403)">
-                                <g id="Group_232" data-name="Group 232">
-                                <path id="Path_267" data-name="Path 267" d="M-321.192,138.631l1920-247.228V301.028l-1920,455.058Z" transform="translate(323.192 2059)" fill="#f32222"/>
-                                <path id="Path_266-2" data-name="Path 266" d="M-318.159-17.14l1920-91.457V715.5l-1920-184.812Z" transform="translate(320.159 2059)" fill="#051133"/>
-                                </g>
-                                <g id="Mask_Group_31" data-name="Mask Group 31" clip-path="url(#clip-path)">
-                                <g id="Group_234" data-name="Group 234" transform="translate(-5.126 59)">
-                                    <image id="Artboard_105_2x-100" data-name="Artboard 105@2x-100" width="750.128" height="562.752" transform="translate(1203 2198.307)" href={icNeon} />
-                                </g>
-                                </g>
-                            </g>
-                            </svg>
-                        
-                        </div>
-                        {/* <div className="position-absolute w-100 d-flex justify-content-end">
-                            <img src={icNeon} alt=""/>
-                        </div> */}
-                        <div className="container landing-benefits p-0">
-                            <div className="col-12 text-center text-primary text-uppercase">
-                                Features
-                            </div>
-                            <div className="col-12 my-2 text-center text-white display5">
-                                Why Choose Us?
-                            </div>
-                            <div className="col-lg-10 offset-lg-1 col-12 mb-3 ">
-                                <div className="row no-gutters">
-                                    <div className="col-xl-4 px-2 col-sm-6 col-12 ">
-                                        <MyCard icon={<Image src={Icon18}/>} title="24/7 Industry Support" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
-                                    </div>
-                                    <div className="col-xl-4 px-2 col-sm-6 col-12">
-                                        <MyCard icon={<Image src={Icon19}/>} title="Dashboard Reporting" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
-                                    </div>
-                                    <div className="col-xl-4 px-2 col-sm-6 col-12">
-                                        <MyCard icon={<Image src={Icon20}/>} title="Wide range of services" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
-                                    </div>
-                                    <div className="col-xl-4 px-2 col-sm-6 col-12">
-                                        <MyCard icon={<Image src={Icon21}/>} title="Secure Transactions" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
-                                    </div>
-                                    <div className="col-xl-4 px-2 col-sm-6 col-12">
-                                        <MyCard icon={<Image src={Icon22}/>} title="Enhanced Performance" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
-                                    </div>
-                                    <div className="col-xl-4 px-2 col-sm-6 col-12">
-                                        <MyCard icon={<Image src={Icon23}/>} title="Simple Pricing" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                
+                </div>
 
+                <div className="container-fluid">
+                    <div className="row flex mt-5 py-lg-5 justify-content-center">
+                        <div className="d-flex position-absolute justify-content-center overflow-hidden w-100" >
+                            <svg  style={{width:svgWidth}} height="864.684" >
+                                <defs>
+                                    <clipPath id="clip-path">
+                                    <path id="Path_266" data-name="Path 266" d="M-318.159-17.14l3840-91.457V715.5l-3840-184.812Z" transform="translate(318.159 2059)" fill="#051133"/>
+                                    </clipPath>
+                                </defs>
+                                <g id="Group_233" data-name="Group 233" transform="translate(-5 -1950.403)">
+                                    <g id="Group_232" data-name="Group 232">
+                                    <path id="Path_267" data-name="Path 267" d="M-321.192,138.631l3840-247.228V301.028l-3840,455.058Z" transform="translate(323.192 2059)" fill="#f32222"/>
+                                    <path id="Path_266-2" data-name="Path 266" d="M-318.159-17.14l3840-91.457V715.5l-3840-184.812Z" transform="translate(320.159 2059)" fill="#051133"/>
+                                    </g>
+                                    <g id="Mask_Group_31" data-name="Mask Group 31" clip-path="url(#clip-path)">
+                                    <g id="Group_234" data-name="Group 234" transform="translate(-5.126 59)">
+                                        <image id="Artboard_105_2x-100" data-name="Artboard 105@2x-100" width="750.128" height="562.752" transform={`translate(${svgWidth-800} 2100.307)`} href={icNeon} />
+                                    </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                        
+                <div className="container landing-benefits">
+                    <div className="row forth">
+                    <div className="col-12 features text-center text-uppercase">
+                        Features
+                    </div>
+                    <div className="col-12 my-2 text-center text-white display5">
+                        Why Choose Us?
+                    </div>
+                    <div className="col-lg-10 offset-lg-1 col-12 mb-3 ">
+                        <div className="row no-gutters">
+                            <div className="col-xl-4 px-2 col-sm-6 col-12 ">
+                                <MyCard icon={<Image src={Icon18}/>} title="24/7 Industry Support" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
+                            </div>
+                            <div className="col-xl-4 px-2 col-sm-6 col-12">
+                                <MyCard icon={<Image src={Icon19}/>} title="Dashboard Reporting" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
+                            </div>
+                            <div className="col-xl-4 px-2 col-sm-6 col-12">
+                                <MyCard icon={<Image src={Icon20}/>} title="Wide range of services" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
+                            </div>
+                            <div className="col-xl-4 px-2 col-sm-6 col-12">
+                                <MyCard icon={<Image src={Icon21}/>} title="Secure Transactions" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
+                            </div>
+                            <div className="col-xl-4 px-2 col-sm-6 col-12">
+                                <MyCard icon={<Image src={Icon22}/>} title="Enhanced Performance" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
+                            </div>
+                            <div className="col-xl-4 px-2 col-sm-6 col-12">
+                                <MyCard icon={<Image src={Icon23}/>} title="Simple Pricing" children="Lorem ipsum kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet dolor sit amet, consetetur sadipscing elitr." iconBg="Green"/>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+        
                 <div className="container-fluid brands my-5 py-2">
                     <div className="marquee">
                     <div className="justify-content-center text-center d-flex abc mx-xl-5 mx-0 scroll">
@@ -217,7 +221,6 @@ export class Landing extends Component {
                     <div className="row mb-5">
                             <Image src={Map}/>
                     </div>
-
                     <div className="row mt-lg-5 mt-2 mt-lg-5">
                         <div className="col-12 pt-3 pt-md-4 first">
                             <div className="welcome">
@@ -227,6 +230,7 @@ export class Landing extends Component {
                         </div>
                     </div>
                 </div>
+
                 <div className="container-fluid mb-2">
                     <div className="row">
                         <div className="col-12">
@@ -234,6 +238,7 @@ export class Landing extends Component {
                         </div>
                     </div>
                 </div>
+
                 <div className="container"> 
                     <div className="row my-5">
                         <div className="col-md-4 col-12 pt-md-5 third mycard ">
@@ -252,9 +257,10 @@ export class Landing extends Component {
                         </div>
                     </div>
                 </div>
+
             </div>
         )
     }
 }
 
-export default Landing
+export default Landing;
