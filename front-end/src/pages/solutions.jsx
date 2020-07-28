7ea940453c1ee7d13352268cc0d79d014d187fd1
Manav <manavchhabraj7@gmail.com>
2020-07-29 02:12:02 +0530

    Mergin files

--- front-end/src/pages/solutions.jsx
+++ front-end/src/pages/solutions.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Head from 'next/head'
 import CardWithIcon from '../components/cards/CardWithIcon';
-import Head from "next/head";
 import {Image} from '@chakra-ui/core'
 
 class OurSolutions extends Component {
