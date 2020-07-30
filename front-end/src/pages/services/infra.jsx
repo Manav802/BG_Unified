ebcf8db37053f14bccb131ea1f19905f7ce7f1d7
ShubhBhardwaj11 <40259519+ShubhBhardwaj11@users.noreply.github.com>
2020-07-31 00:18:35 +0530

    Update infra.jsx

--- front-end/src/pages/services/infra.jsx
+++ front-end/src/pages/services/infra.jsx
@@ -10,7 +10,7 @@ function infra(props) {
                     <div className="row">
                         <div className="col-lg-12 mt-5 text-center py-5">
                             <div className="display2 text-white">
-                                Computing Infrastructure
+                               Computing Infrastructure
                             </div>
                         </div>
                     </div>
@@ -23,11 +23,10 @@ function infra(props) {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-10 text-center offset-lg-1">
-                        <div className="h4 NunitoSans-ExtraBold">Over 50 geographically dispersed sites hosting 
-                                            & 250+ separate pieces of infrastructure.</div>
+                        <div className="h4 NunitoSans-ExtraBold">Server consolidation and virtualization, high availability & clustering, desktop virtualization, and server management. 
+                        </div>
                         <div className="h6 mt-4 text-secondary text-dark">
-                                            Server consolidation and virtualization, high availability & clustering, desktop virtualization, and server management. 
-                                            Infrastructure includes more than 10000 Cores CPU, 10TB RAM.
+                       Infrastructure includes more than 10000 Cores CPU, 10TB RAM.
                         </div>
                     </div>
                 </div>
@@ -42,13 +41,10 @@ function infra(props) {
                     <div className="col-lg-7 d-flex align-items-center px-5">
                         <div>
                         <div className="h5 NunitoSans-Bold text-dark">
-                            Scalability and Management
+                        Geographical Redundant Storage
                         </div>
                         <p className="text-secondary mt-3">
-                                Easily adjust parameters based on deploying your application, physically, virtually or in the cloud. 
-                                Security and Network Management, Data Centre, Management, IT Service Management, Cloud Computing.
-
-                        </p>
+                        over 50 geographically dispersed sites hosting 250+ separate pieces of infrastructure. Geographically sound, redundant, quick and efficient increasing your backup by 60%.</p>
                         </div>
                     </div>
                     
@@ -60,22 +56,37 @@ function infra(props) {
                     <div className="col-lg-7 d-flex align-items-center px-5">
                         <div>
                         <div className="h5 NunitoSans-Bold text-dark">
-                            Higher Asset Utilization with Simplicity
+                        Scalability and Management
                         </div>
                         <p className="text-secondary mt-3">
-                               Implement a flexible architecture designed specifically to meet your unique needs. F5 load balancing solutions ensure scaling your critical applications.
-                        </p>
+                        Easily adjust parameters based on deploying your application, physically, virtually or in the cloud. Security and Network Management, Data Centre, Management, IT Service Management, Cloud Computing.
+              </p>
                         </div>
                     </div>
                     
                 </div>
-            </div>
-        </div>
+                <div className="row">
+                    <div className="col-lg-5 p-5">
+                        <img src="/assets/images/vectors/SVG/firewall.svg" alt=""/>
+                    </div>
+                    <div className="col-lg-7 d-flex align-items-center px-5">
+                        <div>
+                        <div className="h5 NunitoSans-Bold text-dark">
+                       Higher Asset Utilization with Simplicity  
+                        </div>
+                        <p className="text-secondary mt-3">
+                        Implement a flexible architecture designed specifically to meet your unique needs. F5 load balancing solutions ensure scaling your critical applications.
+                            </p>
+                        </div>
+                    </div>
+                    
+                </div>
+                
         <div className="section">
             <div className="container">
                 <div className="row">
                     <div className="h2 NunitoSans-ExtraBold col-12 text-center">
-                        Ready to try out our <br className="d-none d-md-block" /> Infrastructure Services?
+                        Ready to try out our <br className="d-none d-md-block" /> UCaaS Services?
                     </div>
                     <div className="h6 col-12 mt-2 text-secondary text-center">
                         BGUS Servers are affordable and dynamically priced.
