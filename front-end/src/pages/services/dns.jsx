87a6d44a3dd09a5aaf9ed6984978a89aec30dd2e
Manav <manavchhabraj7@gmail.com>
2020-07-31 15:59:22 +0530

    fINAL

--- front-end/src/pages/services/dns.jsx
+++ front-end/src/pages/services/dns.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Button } from '@chakra-ui/core'
-
+import Head from 'next/head'
 function main(props) {
     return (
         <div>
+            <Head>
+                <title>DNS - BG Unified</title>
+
+                <link rel="shortcut icon" href="/assets/images/icons/favicon/bg.ico" type="image/x-icon" />
+            </Head>
             <div className="section pb-0 bg-dark">
                 <div className="section">
                     <div className="container">
@@ -33,8 +38,8 @@ function main(props) {
             <div className="section">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-5 p-5">
-                            <img src="/assets/images/Illustrations/Firewall/diversityArtboard1.svg" alt="" />
+                        <div className="col-lg-5 p-5 ">
+                            <img src="/assets/images/Illustrations/Services/Firewall/diversity.svg" alt="" className="max-height2" />
                         </div>
                         <div className="col-lg-7 d-flex align-items-center px-5">
                             <div>
@@ -48,7 +53,7 @@ function main(props) {
                     </div>
                     <div className="flex-lg-row-reverse row mt-5 mt-lg-0">
                         <div className="col-lg-5 p-5">
-                            <img src="/assets/images/Illustrations/Firewall/diversityArtboard1.svg" alt="" />
+                            <img src="/assets/images/Illustrations/Services/Firewall/diversity.svg" alt="" className="max-height2" />
                         </div>
                         <div className="col-lg-7 d-flex align-items-center px-5">
                             <div>
@@ -61,9 +66,9 @@ function main(props) {
 
                     </div>
 
-                    <div className="flex-lg-row-reverse row mt-5 mt-lg-0">
+                    <div className=" row mt-5 mt-lg-0">
                         <div className="col-lg-5 p-5">
-                            <img src="/assets/images/Illustrations/Firewall/diversityArtboard1.svg" alt="" />
+                            <img src="/assets/images/Illustrations/Services/Firewall/diversity.svg" alt="" className="max-height2"/>
                         </div>
                         <div className="col-lg-7 d-flex align-items-center px-5">
                             <div>
