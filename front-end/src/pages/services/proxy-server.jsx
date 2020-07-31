34005b3dc57f2e78cd3c9ce33644c89b79aae50d
Manav <manavchhabraj7@gmail.com>
2020-07-31 16:13:37 +0530

    fINALising changes

--- front-end/src/pages/services/proxy-server.jsx
+++ front-end/src/pages/services/proxy-server.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Button } from '@chakra-ui/core'
-
+import Head from 'next/head'
 function main(props) {
     return (
         <div>
+            <Head>
+                <title>Proxy Server - BG Unified</title>
+
+                <link rel="shortcut icon" href="/assets/images/icons/favicon/bg.ico" type="image/x-icon" />
+            </Head>
             <div className="section pb-0 bg-dark">
                 <div className="section">
                     <div className="container">
@@ -31,7 +36,7 @@ function main(props) {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-5 p-5">
-                            <img src="/assets/images/Illustrations/Firewall/diversityArtboard1.svg" alt="" />
+                            <img src="/assets/images/Illustrations/Services/Firewall/diversity.svg" alt="" className="max-height2" />
                         </div>
                         <div className="col-lg-7 d-flex align-items-center px-5">
                             <div>
@@ -45,7 +50,7 @@ function main(props) {
                     </div>
                     <div className="flex-lg-row-reverse row mt-5 mt-lg-0">
                         <div className="col-lg-5 p-5">
-                            <img src="/assets/images/Illustrations/Firewall/diversityArtboard1.svg" alt="" />
+                            <img src="/assets/images/Illustrations/Services/Firewall/diversity.svg" alt="" className="max-height2" />
                         </div>
                         <div className="col-lg-7 d-flex align-items-center px-5">
                             <div>
@@ -59,9 +64,9 @@ function main(props) {
 
                     </div>
 
-                    <div className="flex-lg-row-reverse row mt-5 mt-lg-0">
+                    <div className="row mt-5 mt-lg-0">
                         <div className="col-lg-5 p-5">
-                            <img src="/assets/images/Illustrations/Firewall/diversityArtboard1.svg" alt="" />
+                            <img src="/assets/images/Illustrations/Services/Firewall/diversity.svg" alt="" className="max-height2" />
                         </div>
                         <div className="col-lg-7 d-flex align-items-center px-5">
                             <div>
