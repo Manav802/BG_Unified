614bf1482efc121685d37813944df8e8d270219f
Manav802 <manavchhabrj7@gmail.com>
2020-08-29 12:31:32 +0530

    imagegroup component changes

--- front-end/src/pageBuilder/components/ImageGroup.jsx
+++ front-end/src/pageBuilder/components/ImageGroup.jsx
@@ -3,7 +3,7 @@ import { Image, Flex } from "@chakra-ui/core";
 
 const ImageGroup = (props) => {
     const { images, ...other } = props;
-    const allImages = props.images.map((item, index) => <Image src={item} {...other} key={"image" + index}></Image>);
+    const allImages = props.images.map((item, index) => <Image src={item} minW={["119.5px", "135.5px", "175px", "231px", "283px"]} width="auto" key={"image" + index}  {...other}></Image>);
     return (
         < Flex justify="center" textAlign="center" mx={[0, 0, 0, 0, 5]}>
             {allImages}
