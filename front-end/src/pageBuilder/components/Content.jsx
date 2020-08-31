3b5dfbbc735c5788f8c3272e1ab539accbd93b3a
Vineet Yadav <vineetyadav8588@gmail.com>
2020-08-31 22:32:00 +0530

    dns changes

--- front-end/src/pageBuilder/components/Content.jsx
+++ front-end/src/pageBuilder/components/Content.jsx
@@ -6,7 +6,7 @@ function Content(props) {
         <>
             {props.data.map(({fontSize="lg",title,titleSize="lg",titleFont, styleType="disc", opacity=1, spacingY=2, list, text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ratione expedita tenetur nobis provident quas deleniti soluta maiores quos dolorum velit, possimus incidunt iste doloremque fuga laborum eaque ad iusto.", ...props}) => (
                 <>
-                <Heading fontFamily={titleFont} my={4} size={titleSize}>{title}</Heading>
+                <Heading fontFamily={titleFont} my={4} fontSize={titleSize}>{title}</Heading>
                 {text && <Text 
                     fontSize={fontSize} 
                     opacity={opacity}
--- front-end/src/pageBuilder/pages/solutions/dns.jsx
+++ front-end/src/pageBuilder/pages/solutions/dns.jsx
@@ -33,16 +33,19 @@ export default [
         blend: "overlay",
         bg: "dark.500",
         color: "white",
-        rowDistance: 2,
-        gap: 2,
+        rowDistance: 8,
+        gap: 6,
         body: [
             {
+                py:2,
+                my:6,
                 width: [1/2],
                 type: "content",
                 data: [{
                     title: "For a stronger Web Identity",
                     textAlign: "left",
                     fontSize: "xl",
+                    titleSize:"32px",
                     
                     text: "BG Unified Solutions give you the power to easily deploy, efficiently monitor, and transparently scale the applications that your business requires to run and to interact with customers and partners—while also mitigating the risk of security breaches."
                     
@@ -100,5 +103,39 @@ export default [
             }]
         }]
     },
+    {
+        py: 0,
+        body: [{
+            width: "100%",
+            type: "accordion",
+            data: {
+                fontSize: "md",
+                content: [{
+                    title: "Choice between On-Premises & Cloud",
+                    description: "On-premise or cloud-based, that helps secure your IT environment quickly. The mainstay of Enterprise Security. Blocks malicious traffic and allows legitimate traffic to flow without any hindrance..",
+                    image: "/assets/images/Illustrations/Varied_Web_Hosting_Solutions.svg"
+                },
+                {
+                    title: "Fast and easy Deployment",
+                    description: "Rapid evolvement, easily deployable with tremendous speed. Proven deployment methodology that integrates and extends your existing security infrastructure without capex.",
+                    image: "/assets/images/Illustrations/FlexibilityAndScalabilty_AppDev.svg"
+                },
+                {
+                    title: "Advanced Technology Stack",
+                    description: "Advanced Malware Protection, Anti-Virus Detection, Uniform resource Locator Filtering, User-Based Policy and Advanced Logging facilities, SSL Virtual Private Network’s included.",
+                    image: "/assets/images/Illustrations/ReliableAndSecure_CloudExchangeConnectivity.svg"
+                },]
+            }
+        }]
+    },
+    {
+        my:0,
+        title: "Customizable Data Protection for your Business",
+        description: "Migrate your legacy production Application Environments to Next-Gen Data Centres and enable your journey to the cloud.",
+        bgImage: "url(https://images.pexels.com/photos/325229/pexels-photo-325229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)",
+        endingButton: {color: "dark.500"},
+        endingButtontitle: "Lets get IT Done!",
+        color: "light.500",
+    }
 
 ]
