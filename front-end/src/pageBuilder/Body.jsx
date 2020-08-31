dd8d3684bdd0a617c1f98c936bec50f353b47aac
Manav802 <manavchhabrj7@gmail.com>
2020-08-31 16:58:15 +0530

    Complete alternaterow component

--- front-end/src/pageBuilder/Body.jsx
+++ front-end/src/pageBuilder/Body.jsx
@@ -8,53 +8,58 @@ import Feature from './components/Feature';
 import NumberCount from './components/NumberCount';
 import AccordionWithImage from './components/AccordionWithImage';
 import Service from '../components/cards/Service';
+import AlternateRow from './components/AlternateRow';
 
-function Body({gap = 4, data, type, width, ...props}){
-    function giveMeComponent(){
-        switch(type){
+function Body({ gap = 4, data, type, width, ...props }) {
+    function giveMeComponent() {
+        switch (type) {
             case "content":
-            return (
-                <Content data={data}></Content>
-            );
+                return (
+                    <Content data={data}></Content>
+                );
             case "video":
-            return (
-                <Video data={data}></Video>
-            )
+                return (
+                    <Video data={data}></Video>
+                )
             case "list":
-            return (
-                <MyList data={data}></MyList>
-            )
+                return (
+                    <MyList data={data}></MyList>
+                )
             case "imageGroup":
-            return (
-                <ImageGroup {...data}></ImageGroup>
-            )
+                return (
+                    <ImageGroup {...data}></ImageGroup>
+                )
             case "feature":
-            return (
-                <Feature {...data}></Feature>   
-            )
+                return (
+                    <Feature {...data}></Feature>
+                )
             case "numberCount":
-            return(
-                <NumberCount {...data}></NumberCount>
-            )
+                return (
+                    <NumberCount {...data}></NumberCount>
+                )
             case "accordion":
-            return(
-                <AccordionWithImage {...data}></AccordionWithImage>
-            )
+                return (
+                    <AccordionWithImage {...data}></AccordionWithImage>
+                )
             case "service":
-            return(
-                <Service service={data}></Service>
-            )
+                return (
+                    <Service service={data}></Service>
+                )
             case "pricing":
-            return (
-                <>
-                {data}
-                </>
-            )
+                return (
+                    <>
+                        {data}
+                    </>
+                )
+            case "altrow":
+                return (
+                    <AlternateRow {...data}></AlternateRow>
+                )
         }
     }
 
     return (
-        <Box px={[6, 6, gap/2]} my={4} w={width} {...props}>
+        <Box px={[6, 6, gap / 2]} my={4} w={width} {...props}>
             {giveMeComponent()}
         </Box>
     );
--- front-end/src/pageBuilder/pages/solutions/backup.jsx
+++ front-end/src/pageBuilder/pages/solutions/backup.jsx
@@ -28,14 +28,14 @@ export default [
             type: "imageGroup",
             data: {
                 images: ["/assets/images/os/redhat.png", "/assets/images/os/windows10.png", "/assets/images/os/windowsServer.png"],
-            
+
                 height: "96px",
                 width: ["128px"],
                 objectFit: "contain",
                 spacing: ["5%", "6%", "12%"]
-                  
+
             }
-            
+
         }]
     },
     {
@@ -44,42 +44,69 @@ export default [
     {
         body: [{
             width: "100%",
-            type: "accordion",
+            type: "altrow",
             data: {
-                fontSize: "lg",
-                content: [{
+                textWidth: ["100%", "100%", "60%",],
+                descriptionStyles: {
+                    opacity: 0.7,
+                    textAlign: "justify",
+                    mr: ["0", "0", "40px"],
+                    fontSize: "lg"
+                },
+                titleStyles: { fontSize: "xl" },
+                imageStyles: { maxWidth: ["50%", "50%", "100%"], mx: "auto", mt: "30px" },
+                direction: ["column", "column", "row"],
+                content: {
+                    image: "/assets/images/Illustrations/low_pricing.svg",
                     title: "Flexible and Scalable Service",
-                    description: "For any of your organization department, our BaaS offering provides a simplified IT management and a highly scalable platform. Irrespective of the end-user, everyone gets to access to the latest and the greatest backup technology stack. Customized and tailor solutions to meet client needs with Local end-market support services and Local language capabilities",
-                    image: "/assets/images/Illustrations/low_pricing.svg"
-                }]
+                    description: "For any of your organization department, our BaaS offering provides a simplified IT management and a highly scalable platform. Irrespective of the end-user, everyone gets to access to the latest and the greatest backup technology stack. Customized and tailor solutions to meet client needs with Local end-market support services and Local language capabilities"
+                }
             }
         }]
     },
     {
         body: [{
             width: "100%",
-            type: "accordion",
+            type: "altrow",
             data: {
-                fontSize: "lg",
-                content: [{
+                textWidth: ["100%", "100%", "60%",],
+                descriptionStyles: {
+                    opacity: 0.7,
+                    textAlign: "justify",
+                    ml: ["0", "0", "40px"],
+                    fontSize: "lg"
+                },
+                titleStyles: { fontSize: "xl", ml: ["0", "0", "40px"] },
+                imageStyles: { maxWidth: ["50%", "50%", "100%"], mx: "auto", mt: "30px" },
+                direction: ["column", "column", "row-reverse"],
+                content: {
+                    image: "/assets/images/Illustrations/low_pricing.svg",
                     title: "Cost-Effective Solution",
-                    description: "For your business, we are offering and providing a flat billing structure. It's designed for pay as you go model that shifts your costs from capex to opex. You will have very simplified and predictable expenses and have the option of rolling any upfront or one-off expenses into your monthly payment.",
-                    image: "/assets/images/Illustrations/low_pricing.svg"
-                }]
+                    description: "For your business, we are offering and providing a flat billing structure. It's designed for pay as you go model that shifts your costs from capex to opex. You will have very simplified and predictable expenses and have the option of rolling any upfront or one-off expenses into your monthly payment."
+                }
             }
         }]
     },
     {
         body: [{
             width: "100%",
-            type: "accordion",
+            type: "altrow",
             data: {
-                fontSize: "lg",
-                content: [{
+                textWidth: ["100%", "100%", "60%",],
+                descriptionStyles: {
+                    opacity: 0.7,
+                    textAlign: "justify",
+                    mr: ["0", "0", "40px"],
+                    fontSize: "lg"
+                },
+                titleStyles: { fontSize: "xl" },
+                imageStyles: { maxWidth: ["50%", "50%", "100%"], mx: "auto", mt: "30px" },
+                direction: ["column", "column", "row"],
+                content: {
+                    image: "/assets/images/Illustrations/low_pricing.svg",
                     title: "Back it Up ",
-                    description: "Geographically sound, redundant, quick and efficient increasing your backup by 60%. Highly advanced, available and multiple data centre locations offering multiple IP types globally, in a clustered or isolated environment.",
-                    image: "/assets/images/Illustrations/low_pricing.svg"
-                }]
+                    description: "Geographically sound, redundant, quick and efficient increasing your backup by 60%. Highly advanced, available and multiple data centre locations offering multiple IP types globally, in a clustered or isolated environment."
+                }
             }
         }]
     },
