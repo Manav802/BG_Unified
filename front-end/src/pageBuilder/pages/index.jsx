66e00c38b4cde3e6e62d00fb2d99d5b242118a46
Vineet Yadav <vineetyadav8588@gmail.com>
2020-09-01 15:27:33 +0530

    platform structure

--- front-end/src/pageBuilder/pages/index.jsx
+++ front-end/src/pageBuilder/pages/index.jsx
@@ -2,7 +2,9 @@ import storage from "./solutions/storage-unit"
 import infra from "./solutions/infra"
 import backup from "./solutions/backup"
 import dns from "./solutions/dns"
+import platform from "./solutions/platform"
 module.exports.storage = storage
 module.exports.infra = infra
 module.exports.backup = backup
 module.exports.dns = dns
+module.exports.platform = platform
