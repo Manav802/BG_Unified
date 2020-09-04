8c9eec02e3db25716136f3c3bc991cb93259b570
Manav802 <manavchhabrj7@gmail.com>
2020-09-04 22:07:37 +0530

    Adding typewriter in

--- front-end/src/pageBuilder/RenderSection.jsx
+++ front-end/src/pageBuilder/RenderSection.jsx
@@ -6,13 +6,21 @@ import Description from './Description';
 import Row from './Row';
 import Body from './Body';
 import Link from 'next/link';
+import Typewriter from 'typewriter-effect';
 
-function RenderSection({ title,typeWriter_title, titleStyle, color = "dark.500", align = "center", description, containerWidth = ["100%", "100%", "720px", "960px", "1140px"], descriptionStyle, endingButtonTitle, endingButton, slider,endingButtonLink, sliderOption, gap = 4, rowDistance = 0, body, ...props }) {
+function RenderSection({ title, typeWriter_title, titleStyle, color = "dark.500", align = "center", description, containerWidth = ["100%", "100%", "720px", "960px", "1140px"], descriptionStyle, endingButtonTitle, endingButton, slider, endingButtonLink, sliderOption, gap = 4, rowDistance = 0, body, ...props }) {
     return (
         <>
             <Section containerWidth={containerWidth} color={color} align={align} {...props}>
                 {title && <Title {...titleStyle}>{title}</Title>}
-                {typeWriter_title && <Title {...titleStyle}><span>{typeWriter_title.before}</span><span>{typeWriter_title.typed}</span><span>{typeWriter_title.after}</span></Title>}
+                {typeWriter_title && <Title {...titleStyle}>
+                    <span>{typeWriter_title.before}</span>
+                    <Typewriter options={{
+                        strings: typeWriter_title.typed,
+                        autoStart: true
+                    }}></Typewriter>
+                    <span>{typeWriter_title.after}</span>
+                </Title>}
                 {description && <Description {...descriptionStyle}>{description}</Description>}
                 {body && <Row rowDistance={rowDistance}>
                     {body && body.map((b, index) => (
