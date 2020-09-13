68f976d68d40a817595c4c77e44c26131e2ccc14
ShubhBhardwaj11 <40259519+Shubh29nov@users.noreply.github.com>
2020-09-13 21:30:20 +0530

    Completed case study

---
+++ front-end/src/pageBuilder/pages/newsroom/f5top.jsx
@@ -0,0 +1,33 @@
+export default [
+    {
+        type: "content",
+        fontSize: "xl",
+        data: [
+            {
+                text: "F5 Configuration was redesigned and implemented in a recommended way. A lot of IRULES/POOLS defined to achieve the desired configuration requirements. Health Monitors were rewritten to provide the best monitor for the servers in different pools.",
+                textAlign: "justify"
+            },
+            {
+                text: "The project involved the integration of Big IPs to the newly implemented DC. Design a Load Balancing topology for web servers running multiple instances of TOMCAT. A couple of applications are SSL based and others are Non-SSL based. Implementation of advanced features of F5 including Application Monitoring, Intrusion Prevention, Application Inspection and dynamically powering on the VMs based on the load or other criteria.",
+                textAlign: "justify"
+            },
+            {
+                text: "F5 load balancing solutions employ F5 iApps that help them deploy 10x to 100x faster with 95 per cent fewer configuration mistakes.",
+                textAlign: "justify"
+            },
+            {
+                text: "The F5 application-centric view allowed us to set up customized network services with ease. And by managing application services rather than the individual networking components and configurations, we could dramatically speed deployment, lower operational expenses, and streamline IT operations." ,
+                textAlign: "justify"
+            },
+            {
+                text: "F5 application-centric load balancing solutions ensured that our customers and partners have access to the applications they need—whenever they need them—by monitoring the health and performance of individual servers in real-time.",
+                textAlign: "justify"
+            },
+           
+            {
+                text: "Implementation of PCI Compliance and configuration of ASM to protect applications from different attacks.",
+                textAlign: "justify"
+            },
+                   ]
+    }
+]
