87f59a80081a22ddd0c0bd92af676e85492d977a
Yuvraj Midha <inventive8983@gmail.com>
2020-09-13 18:26:42 +0530

    deleted out folder

--- front-end/out/_next/static/rK-ZJhYgn07opUO5bduit/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblogs\u002F[bid]","\u002Fnewsroom\u002F[bid]","\u002Fsolutions\u002F[name]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- front-end/out/assets/js/chatbot.js
+++
@@ -1,7 +0,0 @@
-    window.__be = window.__be || {};
-    window.__be.id = "5f33b3639413eb0007ca8f83";
-    (function() {
-        var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
-        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
-    })();
