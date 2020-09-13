69e6bba69b27c3a47bbb7aeb00176bb4023adbde
Vineet Yadav <vineetyadav8588@gmail.com>
2020-09-13 13:33:55 +0530

    case sensitive

--- front-end/src/pageBuilder/components/Feature.jsx
+++ front-end/src/pageBuilder/components/Feature.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Flex, Icon,Image, Heading, Text, Box } from '@chakra-ui/core';
 import { ReactSVG } from 'react-svg'
-import SVG from '../../components/svg/svg';
+import SVG from '../../components/svg/SVG';
 
 function Feature({image, icon, size = "48px", title,iconColor = "dark.500" ,color="dark.500", svg, titleSize = "lg", description, align="center",textOpacity,titleFont,titleColor, ...props}) {
     return (
