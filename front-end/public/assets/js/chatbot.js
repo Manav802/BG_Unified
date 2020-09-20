737e3e5d7082c6637b73d9f5aef525bcb8bc127c
Keysang Yonthan <keysangkeysang@gmail.com>
2020-09-20 14:55:43 +0530

    saurab chatbot

--- front-end/public/assets/js/chatbot.js
+++ front-end/public/assets/js/chatbot.js
@@ -1,11 +1,11 @@
 window.watsonAssistantChatOptions = {
-    integrationID: "094470f4-bbc5-405b-a348-fb7f2d25dee2", // The ID of this integration.
-    region: "eu-gb", // The region your integration is hosted in.
-    serviceInstanceID: "a9e348ca-f848-400f-be29-9b8b7e5ef613", // The ID of your service instance.
-    onLoad: function(instance) { instance.render(); }
-  };
+  integrationID: "0db64e36-dd73-426a-ac43-f589d52c31bf", // The ID of this integration.
+  region: "us-south", // The region your integration is hosted in.
+  serviceInstanceID: "096c37aa-8126-42e2-956e-6573fe01fbd0", // The ID of your service instance.
+  onLoad: function(instance) { instance.render(); }
+};
 setTimeout(function(){
-  const t=document.createElement('script');
-  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
-  document.head.appendChild(t);
+const t=document.createElement('script');
+t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+document.head.appendChild(t);
 });
