bc14577aae66ba7a72d10bd8ce8d2c311dc052a4
Anuj Bansal <bansalanuj9@gmail.com>
2020-09-24 15:23:18 +0530

    Update server.js

--- front-end/server.js
+++ front-end/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const next = require('next')
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
