07d18316f1cfcde39ed0c3eae52ef13047b5dfd3
Vineet Yadav <vineetyadav8588@gmail.com>
2020-09-28 18:53:17 +0530

    deleted out

--- front-end/out/_next/static/HeTkLBhQncihvF-LuCzhc/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fnewsroom\u002F[bid]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- front-end/out/_next/static/chunks/31.83b37ce9187ab2c4614a.js
+++
@@ -1,1 +0,0 @@
-(window.jpgackJsonp=window.jpgackJsonp||[]).push([[31],{"Z/3v":function(n,w){}}]);
