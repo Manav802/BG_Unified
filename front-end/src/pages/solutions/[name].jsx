d9dcd5b92695a799ed18bfc7d7cd62fed9d4a976
Vineet Yadav <vineetyadav8588@gmail.com>
2020-09-28 18:58:36 +0530

    commit

--- front-end/src/pages/solutions/[name].jsx
+++ front-end/src/pages/solutions/[name].jsx
@@ -37,8 +37,8 @@ function Solutions(props) {
                 </Head>
                 {/* <Image alt={service.title} opacity=".3" className="bg-image" src={service.bgImage}/> */}
                 <picture>
-                    <source srcSet={require(`../../../public${service.bgImage}?webp`)} type="image/webp" />
-                    <source srcSet={require(`../../../public${service.bgImage}`)} type="image/jpeg" />
+                    <source srcSet={require(`../../../public${service.bgImage}?webp?lqip`)} type="image/webp" />
+                    <source srcSet={require(`../../../public${service.bgImage}?lqip`)} type="image/jpeg" />
                     <img
                     className="bg-image"
                     style={{opacity:".3"}}
