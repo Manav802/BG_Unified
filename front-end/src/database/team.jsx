f50f7b97f0ff73d05624e5c1fe75ff541e692861
Yuvraj Midha <inventive8983@gmail.com>
2020-10-01 02:21:37 +0530

    team database

--- front-end/src/database/team.jsx
+++ front-end/src/database/team.jsx
@@ -1,7 +1,7 @@
 export default [
     {
-        name: "Vineet",
-        Description: "Front end web developer in the team, able to handle multiple tasks on a daily basis , technology enthusiast and eager to learn new skills.",
+        name: "Vineet Yadav",
+        description: "Front end web developer of the side. Multi-tasking, curious, and efficiency are some of his personality traits. Keeps on working to enhance his technical skillset. Currently pursuing Engineering in the field of Computer Science.",
         links : {
             github:"https://github.com/vineet8588",
             linkedin:"https://www.linkedin.com/in/vineet-yadav-abb6b3194",
@@ -10,11 +10,11 @@ export default [
             blogger:"",
             youtube:""
         },
-        imgSrc:""
+        username: "vineet"
     },
     {
         name: "Keysang Yonthan",
-        Description: "paste description here",
+        description: "Budding Engineer in the field of Computer Science. An explorer by nature and loves exploring new stuff. A beginner in frontend web technology stack and currently exploring the field of UI/UX and ReactJS.",
         links : {
             github:"https://github.com/keysangyonthan",
             linkedin:"https://www.linkedin.com/in/keysang-yonthan-390643180",
@@ -23,6 +23,25 @@ export default [
             blogger:"",
             youtube:"https://www.youtube.com/channel/UCHPEV-bfHD-myQVLO-aOemg"
         },
-        imgSrc:""
+        username: "keysang"
     },
+    {
+        name: "Vishu Chhabra",
+        description: "Engineer in making. Backend Developer and Database Manager. Apart from this, passionate technical personnel showcases his talents in various hackathons and technical events. Also, works his way through competitive coding.",
+        username: "vishu"
+
+    },
+    {
+        name: "Shubh Bhardwaj",
+        description: "Content Developer in the Team. Thorough with the idea of presenting quality SEO content. Front End Development is what he's been efficient in. Digital Marketing and ardent Travel Blogging, to list a few of his domain expertise!!",
+        username: "shubh"
+    },
+    {
+        name: "Sparsh Gupta",
+        description: "Graphic designer and Illustrator of the squad. Multitasker and always curious to explore new things and gather knowledge on various topics. Currently exploring fields of UI/UX designing. ",
+        username: "sparsh"
+    },
+    {
+
+    }
 ]
