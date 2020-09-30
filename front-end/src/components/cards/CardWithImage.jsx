b2d0c1123f8034874a0cc677a21da8201845ac24
Manav802 <manavchhabrj7@gmail.com>
2020-09-30 14:25:32 +0530

    Modal height drawer

--- front-end/src/components/cards/CardWithImage.jsx
+++ front-end/src/components/cards/CardWithImage.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { Box, Image, Heading, Text, Button, ButtonGroup, Flex, Link } from "@chakra-ui/core";
+import { Box, Image, Heading, Text, ButtonGroup, Flex } from "@chakra-ui/core";
 
-function CardWithImage({columns="auto", height="216px", ...props}) {
+function CardWithImage({ columns = "auto", height, ...props }) {
   return (
-    
+
     <Flex direction="column" bg="white" rounded={8} overflow="hidden" boxShadow="xl" height="100%">
-      
-          <img
-          height={height}
-          style={{width: "100%"}}
-          src={props.src}
-          alt={props.src}
-          />
-      
-      <Box  py={[3,6]} px={[3,6]}>
-        <Heading fontSize={["md","2xl"]} lineHeight={1.5} mt={[2,1]} mb={[1, 2]} letterSpacing={1} fontFamily="Nexa Bold">{props.title}</Heading>
+
+      <Image
+        height={height || "216px"}
+        style={{ width: "100%" }}
+        src={props.src}
+        alt={props.src}
+      />
+
+      <Box py={[3, 6]} px={[3, 6]}>
+        <Heading fontSize={["md", "2xl"]} lineHeight={1.5} mt={[2, 1]} mb={[1, 2]} letterSpacing={1} fontFamily="Nexa Bold">{props.title}</Heading>
         {props.description && <Text textAlign="justify" fontSize="lg" mt={2}>{props.description}</Text>}
         {props.children}
         <ButtonGroup spacing={4}>
-            {props.actions}
+          {props.actions}
         </ButtonGroup>
       </Box>
     </Flex>
