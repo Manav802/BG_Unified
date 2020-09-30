11eef9e2c4dd9ff54285e6a03f55825588ddfb5c
Vineet Yadav <vineetyadav8588@gmail.com>
2020-09-30 12:19:22 +0530

    team database template

---
+++ front-end/src/database/team.jsx
@@ -0,0 +1,26 @@
+export default [
+    {
+        name: "Vineet",
+        Description: "Front end web developer in the team, able to handle multiple tasks on a daily basis , technology enthusiast and eager to learn new skills.",
+        links : {
+            github:"https://github.com/vineet8588",
+            linkedin:"https://www.linkedin.com/in/vineet-yadav-abb6b3194",
+            behance:"",
+            dribble:"",
+            blogger:"",
+            youtube:""
+        }
+    },
+    {
+        name: "Keysang Yonthan",
+        Description: "paste description here",
+        links : {
+            github:"https://github.com/keysangyonthan",
+            linkedin:"https://www.linkedin.com/in/keysang-yonthan-390643180",
+            behance:"",
+            dribble:"",
+            blogger:"",
+            youtube:"https://www.youtube.com/channel/UCHPEV-bfHD-myQVLO-aOemg"
+        }
+    },
+]
