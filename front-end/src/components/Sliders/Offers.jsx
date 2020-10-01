1e8d2313daef3ff8061213a4315f0661ce2d5066
Yuvraj Midha <inventive8983@gmail.com>
2020-10-01 11:44:15 +0530

    final release build 1.1

--- front-end/src/components/Sliders/Offers.jsx
+++ front-end/src/components/Sliders/Offers.jsx
@@ -22,7 +22,7 @@ function main({height = "296px", ...props}) {
                 <Grid templateColumns={["100%","128px auto","192px auto","256px auto","40% 60%"]} p={[8,8,12]}>
                     <Image height={height} alt="Free Healthcare Services" justifySelf="center" src="/assets/images/vectors/covid.svg" padding={[4,0]}  width={["60%","100%"]}></Image>
                     <Flex direction="column" justify="center" px={[3,5,12]}>
-                        <a className="link"><Heading fontSize={["xl","xl","2xl","3xl"]} fontFamily="Nexa Bold">Free Services* for three months to Health and Safety related Businesses.<Icon name="chevron-right"></Icon></Heading></a>
+                        <Heading fontSize={["xl","xl","2xl","3xl"]} fontFamily="Nexa Bold">Free Services* for three months to Health and Safety related Businesses.</Heading>
                         <Text mt={3} opacity={.7}>Let us help you if you have limited budgets, and because of COVID, your current infrastructure can't handle the load. Dial our direct line +61 2 9095 0000. We will get things in motion very quickly.</Text>
                     </Flex>
                 </Grid>
@@ -33,7 +33,7 @@ function main({height = "296px", ...props}) {
                 <Grid templateColumns={["100%","128px auto","192px auto","256px auto","40% 60%"]} p={[8,8,12]}>
                     <Image height={height} alt="Free Healthcare Services" justifySelf="center" src="/assets/images/vectors/GPU_Offer-01.svg" padding={[4,0]}  width={["60%","100%"]}></Image>
                     <Flex direction="column" justify="center" px={[3,5,12]}>
-                        <a className="link"><Heading fontSize={["xl","xl","2xl","3xl"]} fontFamily="Nexa Bold">BGUS is launching GPUaaS through multiple Geo-locations and with Bundled Solutions!!<Icon name="chevron-right"></Icon></Heading></a>
+                        <Heading fontSize={["xl","xl","2xl","3xl"]} fontFamily="Nexa Bold">BGUS is launching GPUaaS through multiple Geo-locations and with Bundled Solutions!!</Heading>
                         <Text mt={3} opacity={.7}>GPUaaS will be offered through multiple geo-locations and to make it more profitable and economical for your business; we are ready to bundle the offering with IaaS, Storage As A Service, UCaaS, DNSaaS, SIPaaS and more.
 
                         </Text>
@@ -46,7 +46,7 @@ function main({height = "296px", ...props}) {
                 <Grid templateColumns={["100%","128px auto","192px auto","256px auto","40% 60%"]} p={[8,8,12]}>
                     <Image height={height} alt="Free Healthcare Services" justifySelf="center" src="/assets/images/vectors/SOC_Offer-01.svg" padding={[4,0]} width={["60%","100%"]}></Image>
                     <Flex direction="column" justify="center" px={[3,5,12]}>
-                        <a className="link"><Heading fontSize={["xl","xl","2xl","3xl"]} fontFamily="Nexa Bold">Entirely hosted SOCaaS within BGUS DC locations, supported with different technologies!!<Icon name="chevron-right"></Icon></Heading></a>
+                        <Heading fontSize={["xl","xl","2xl","3xl"]} fontFamily="Nexa Bold">Entirely hosted SOCaaS within BGUS DC locations, supported with different technologies!!</Heading>
                         <Text mt={3} opacity={.7}>BGUS will host SOCaaS entirely within its own DC locations, and the service will be offered with a unique combination of multiple technologies with every single event monitored by a team of highly skilled individuals.</Text>
                     </Flex>
                 </Grid>
