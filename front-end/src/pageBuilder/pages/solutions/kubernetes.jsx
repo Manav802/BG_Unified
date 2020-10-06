0f3b10c2a9e03d95283f29ae3acfb2143e5da2fb
ShubhBhardwaj11 <40259519+Shubh29nov@users.noreply.github.com>
2020-10-06 17:09:51 +0530

    commit

--- front-end/src/pageBuilder/pages/solutions/kubernetes.jsx
+++ front-end/src/pageBuilder/pages/solutions/kubernetes.jsx
@@ -3,47 +3,13 @@ export default [
         typeWriter_title: {
             before: "",
             typed: ["Integration", "Interaction", "Innovation"],
-            after: "achieved with Kubernetes"
+            after: "achieved with Kubernetes."
         },
-        py: 12,
-        rowDistance: 8,
-        body: [
-        {
-            width:["100%", "100%","100%", 1/2],
-            type: "imageGroup",
-            data: {
-                images: ["/assets/images/services/storage_challenge.jpg"],
-                borderRadius: 8,
-                width : ["100%","100%",'90%'],
-                height: ["300px","300px","400px"],
-                objectFit: "cover"
-            }
+        description: "Groups containers, making up an application into logical units for easy management and discovery. Kubernetes builds upon years of experience running production workloads, combined with best-of-breed ideas and community practices. Positioned VMs to deliver the most comprehensive, enterprise-grade Kubernetes-based portfolio for modern applications.",
+        descriptionStyle: {
+            textAlign: "left"
         },
-        {
-            width: ["100%", "100%","100%", 1/2],
-            type: "content",
-            data: [{
-                textAlign: "justify",
-                fontSize: ["md","lg"],
-                text: <span>Groups containers that make up 
-                an application into logical units for easy 
-                management and discovery. Kubernetes builds 
-                upon 15 years of experience of running production 
-                workloads at Google, combined with best-of-breed 
-                ideas and practices from the community. Positioned
-                 VMs to deliver the most comprehensive, 
-                 enterprise-grade Kubernetes-based portfolio for 
-                 modern applications. Responsibly troubleshooting 
-                 and managing high computing storage, 
-                 persistent / non-persistent volume, volume claims, 
-                 and volume claim policies related to Kubernetes. 
-                 Configuring, managing Demon set, and Init containers.
-                  </span>
-            }]
-        },
-    ]
-    },
-    {
+    
         body: [
             {
                 py: 4,
@@ -56,7 +22,7 @@ export default [
                     image: "/assets/images/icons/color/cluster-computing.svg",
                     title: "Clustering",
                     align: "left",
-                    description: "Troubleshooting the container applications outside Kubernetes clusters (ports, nodeports, service ports, external load balancers). Managing and creating POC  (single master cluster) for demonstration and High-availability cluster with 3 / 5 node master and variable worker nodes with kubeadm."
+                    description: "Troubleshooting the container applications outside Kubernetes clusters (ports, node ports, service ports, external load balancers). Managing and creating POC."
                 }
             },
             {
@@ -70,7 +36,7 @@ export default [
                     image: "/assets/images/icons/color/multiple.svg",
                     title: "Application Multiplicity",
                     align: "left",
-                    description: "Troubleshooting creating a private /global registry and deploying stateful and stateless applications in Kubernetes container like (Jupyter notebook, Drupal, Prometheus, Mongodb, httpd, nginx, JBoss and other web server related applications)."
+                    description: "Troubleshooting creates a private /global registry and deploys stateful and stateless applications in Kubernetes containers like Jupyter notebook, Drupal, etc. "
                 }
             },
             {
@@ -84,7 +50,7 @@ export default [
                     image: "/assets/images/icons/color/package.svg",
                     title: "Container Management",
                     align: "left",
-                    description: "Managing containers using different container tools like dockers, cri-o other than orchestrating containers with Kubernetes. Troubleshooting the container applications outside Kubernetes clusters ( ports, nodeports, service ports, external load balancers)."
+                    description: "Managing containers using different container tools like dockers, cri-o other than orchestrating containers with Kubernetes. Troubleshooting the container applications."
                 }
             },
             {
@@ -98,7 +64,7 @@ export default [
                     image: "/assets/images/icons/color/learn.svg",
                     title: "Skilled Experts",
                     align: "left",
-                    description: "Sufficient knowledge and troubleshooting skills on using different api-plugins used by users or developers. Managing services in Kubernetes by using operators. Configuring, managing Demon set, and Init containers."
+                    description: "Sufficient knowledge and troubleshooting skills in using different API-plugins used by users or developers. We are managing services in Kubernetes by using operators."
                 }
             },
             {
@@ -112,7 +78,7 @@ export default [
                     image: "/assets/images/icons/color/secure.svg",
                     title: "Completely Secure",
                     align: "left",
-                    description: "Adding users and setting permission for application users inside containers using Pod security standards. Managing TLS and CA certificate for Kubernetes. creating and configuring CNI using SDN (software designed network) for a cluster (like Weave net and Calico)."
+                    description: "Adding users and setting permission for application users inside containers using Pod security standards. Managing TLS and CA certificate for Kubernetes. "
                 }
             },
             {
@@ -126,7 +92,7 @@ export default [
                     image: "/assets/images/icons/color/fix.svg",
                     title: "On-Demand Hardware",
                     align: "left",
-                    description: "Get required CPU and VM's along with different flavors of Linux like Redhat, CentOS, Ubuntu as well as with installing and configuring Kubernetes. Managing containers using different container tools like dockers, cri-o other than orchestrating containers with Kubernetes."
+                    description: "Get the required CPU and VM's along with different flavors of Linux like Redhat, CentOS, Ubuntu, and installing and configuring Kubernetes."
                 }
             },
         ]
@@ -152,7 +118,7 @@ export default [
                     fontSize: ["lg","xl"],
                     titleSize: [40,40,48],
                     my:[2,4,8],
-                    text: "We assist customers to go serverless and operate code in more granular, scalable and cost-effective way. Managing containers using different container tools like dockers,cri-o. Managing cluster applications along with configuring CNI. "
+                    text: "We help customers go serverless and operating code in a more granular, scalable, and cost-effective way. We are managing containers using different container tools like dockers,cri-o, and managing cluster applications and configuring CNI."
                     
                 }]
             },
@@ -172,7 +138,7 @@ export default [
                 {
                     width: [1/2,1/3,1/3,1/2,1/3],
                     type: "feature",
-                    data: {align: "center",titleSize:"md", iconColor:"primary.500", svg:"/assets/images/icons/library/shopping/box-3.svg",mt: 4, title: "Containerised"}
+                    data: {align: "center",titleSize:"md", iconColor:"primary.500", svg:"/assets/images/icons/library/shopping/box-3.svg",mt: 4, title: "Containerized"}
                 },
                 {
                     width: [1/2,1/3,1/3,1/2,1/3],
@@ -192,38 +158,31 @@ export default [
             },
         ]   
     },
-    
-    {
-        title: "Why BG Unified Solutions?",
-        py: 12,
-        body: [
-            {
-                type: "image&text",
-                data: {
-                    image: "/assets/images/illustrations/deploy.svg",
-                    title: "Rigorous Troubleshooting",
-                    flexDirection: ["column", "column", "row"],
-                    description: "Troubleshooting the container applications outside Kubernetes clusters ( ports, nodeports, service ports, external load balancers). troubleshooting and managing high computing storage, persistent / non-persistent volume, volume claims, and volume claim policies related to Kubernetes.  troubleshooting skills on using different api-plugins used by users or developers."
-                }
+   
+{
+    title: "Why BG Unified Solutions?",
+    rowDistance: 8,
+    body: [{
+        width: "100%",
+        type: "accordion",
+        data: {
+            fontSize: "md",
+            content: [{
+                title: "Rigorous Troubleshooting",
+                description: "Troubleshooting the container applications outside Kubernetes clusters ( ports, node ports, service ports, external load balancers). Troubleshooting and managing high computing storage, persistent / non-persistent volume, volume claims, and volume claim policies related to Kubernetes. Troubleshooting skills on using different API-plugins used by users or developers.",
+                image: "/assets/images/illustrations/deploy.svg"
             },
             {
-                type: "image&text",
-                data: {
-                    image: "/assets/images/illustrations/Completeness_CloudLogging.svg",
-                    title: "Efficient Resources",
-                    flexDirection: ["column", "column", "row-reverse"],
-                    description: "Kubernetes knows the compute, memory, and storage resources each application needs and schedules instances across the cluster to maximize resource efficiency. Rapid development and iteration with the ease of application deployment and apps and service management."
-                }
+                title: "Efficient Resources",
+                description: "Kubernetes knows the compute, memory, and storage resources each application needs and schedules instances across the cluster to maximize resource efficiency. Rapid development and iteration with the ease of application deployment and apps and service management.",
+                image: "/assets/images/illustrations/Completeness_CloudLogging.svg"
             },
             {
-                type: "image&text",
-                data: {
-                    image: "/assets/images/illustrations/ClientSpecific_AppDev.svg",
-                    title: "Scalability",
-                    flexDirection: ["column", "column", "row"],
-                    description: "Adding users and setting permission for application users inside containers using Pod security standards (like security contexts). Customizable and requirement based VMs and CPUs. Add on-demand instances to your application to meet the predictive and desired outcomes."
-                }
-            },
-        ]
-    }
+                title: "Scalability",
+                description: "Adding users and setting permission for application users inside containers using Pod security standards (like security contexts). Customizable and requirement based VMs and CPUs. Add on-demand instances to your application to meet the predictive and desired outcomes.",
+                image: "/assets/images/illustrations/ClientSpecific_AppDev.svg"
+            },]
+        }
+    }]
+}
 ]
