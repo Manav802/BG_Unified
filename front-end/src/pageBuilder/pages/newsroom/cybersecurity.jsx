789c81e6891da5bd2ffaa7f12576c3d312e38bd6
ShubhBhardwaj11 <40259519+Shubh29nov@users.noreply.github.com>
2020-10-13 15:31:53 +0530

    commit

---
+++ front-end/src/pageBuilder/pages/newsroom/cybersecurity.jsx
@@ -0,0 +1,24 @@
+export default [
+    {
+        type: "content",
+        fontSize: "xl",
+        data: [
+            {
+                text: <span>A very big step from <b>Radware</b> to work closely with <b>BG Unified Solutions</b> to offer a redundant, highly stable, fully managed and highly secured <b>WAF</b> and <b>DDOS</b> Solutions to the end customers collaboratively.</span>,
+                textAlign: "justify"
+            },
+            {
+                text: <span>The evidence of this common goal is the first step of introducing <b>Radware Alteon 6024</b> devices into BG Unified Solutions primary data center location. We will offer <b>WAFaaS</b> and <b>DDOSaaS</b> from this location.</span>,
+                textAlign: "justify"
+            },
+            {
+                text: "As a leader of BG Unified Solutions, I can see the growth in the cybersecurity services to the end customers especially when BG Unified Solutions is very close to be on the Defence Panel in addition to NSW and Victorian State Government Panel",
+                textAlign: "justify"
+            },
+            {
+                text:<span>A big thanks to <b>Yaniv Hoffman, Kash Sharma, Steve Dikshteyn</b> and rest of the Radware team to work towards common goals of high customer satisfaction.</span> ,
+                textAlign: "justify"
+            },
+                   ]
+    }
+]
