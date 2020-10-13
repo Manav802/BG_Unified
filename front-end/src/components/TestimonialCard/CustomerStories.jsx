c755191d99adfb65d60c5badba895696d4e1db25
Manav802 <manavchhabrj7@gmail.com>
2020-10-14 00:37:32 +0530

    final stories

--- front-end/src/components/TestimonialCard/CustomerStories.jsx
+++ front-end/src/components/TestimonialCard/CustomerStories.jsx
@@ -4,8 +4,8 @@ import Container from "../../pageBuilder/Container";
 
 const Testimonial = (testimonialDetails, imageW = "160px") => {
   return (
-    <Box px={1} my={5}>
-      <Box p={6} height="100%" borderWidth={1} rounded={4} >
+    <Box px={5} my={5}>
+      <Box p={6} height="100%" borderWidth={1} rounded={4} minHeight="460px" >
         <Box h="80%">
           <Image src={testimonialDetails.imageUrl} height="80px" maxW={imageW} objectFit="contain"></Image>
           <Text my={8} textAlign="justify" fontWeight={600} lineHeight={1.5}>
@@ -31,20 +31,18 @@ const Testimonial = (testimonialDetails, imageW = "160px") => {
 
 function main({ testimonials }) {
   return (
-    <Container>
-      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-        <Box>
-          {Testimonial(testimonials[0], "112px")}
-          {Testimonial(testimonials[2], "224px")}
-          {Testimonial(testimonials[4])}
-        </Box>
-        <Box>
-          {Testimonial(testimonials[1])}
-          {Testimonial(testimonials[3])}
-          {Testimonial(testimonials[5])}
-        </Box>
-      </Grid>
-    </Container>
+    <Grid templateColumns={"repeat(2,auto)"}>
+      <Box>
+        {Testimonial(testimonials[0], "112px")}
+        {Testimonial(testimonials[2], "224px")}
+        {Testimonial(testimonials[4])}
+      </Box>
+      <Box>
+        {Testimonial(testimonials[1])}
+        {Testimonial(testimonials[3])}
+        {Testimonial(testimonials[5])}
+      </Box>
+    </Grid>
   );
 }
 
