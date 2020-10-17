ae0775a310d0a685dcfe5a520d8331072d59b491
Vineet Yadav <vineetyadav8588@gmail.com>
2020-10-17 16:01:30 +0530

    minor

--- front-end/src/pages/stories.jsx
+++ front-end/src/pages/stories.jsx
@@ -6,6 +6,7 @@ import Title from "../pageBuilder/Title";
 import Testimonial from "../components/TestimonialCard/Stories";
 import TESTIMONIALS from "../database/testimonials";
 import Row from "../pageBuilder/Row";
+import Link from "next/link";
 function main() {
     return <>
         <Head><title>Customer Stories | BG Unified Solutions</title></Head>
