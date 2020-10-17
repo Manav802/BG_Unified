9c8f2ec998a99d76a4ff47045bb4fa3ae18f6e30
Vineet Yadav <vineetyadav8588@gmail.com>
2020-10-17 18:00:45 +0530

    sdwan

--- front-end/src/pageBuilder/pricing/SdwanPricing.jsx
+++ front-end/src/pageBuilder/pricing/SdwanPricing.jsx
@@ -18,7 +18,7 @@ function SdwanPricing(props) {
     const [switch1, setSwitch1] = React.useState(false);
     const [switch2, setSwitch2] = React.useState(false);
     const handleSwitch = (swi) => swi ? ",Virtual" : ",Physical";
-
+    const fortnietFeatures = ["One WAN Link", "3G/4G Failover Configuration", "Multi-Path Controller & Link Health Monitoring","Dual WAN","Load-balancing & Bandwidth Sharing", "Preferential Policy Routes","QoS Provisioning","Application Prioritization","VPN Failover to data center","Performance SLA","Traffic Shaping"]
     return (
                 <div id="pricing" className="container">
                     <div className="row justify-content-center">
@@ -121,68 +121,52 @@ function SdwanPricing(props) {
                                             bottom
                                         >
                                             <div className="row no-gutters justify-content-center">
-                                                <div className="col-lg-5 my-3">
+                                                <div className="col-lg-4 my-3">
                                                     <PricingCard
-                                                        featureList={features}
-                                                        icon="/assets/images/icons/theme/globe.svg"
-                                                        title="Single"
+                                                        featureList={fortnietFeatures.slice(0,3)}
+                                                        excludingList={fortnietFeatures.slice(3,11)}
+                                                        icon="/assets/images/icons/theme/origami.svg"
+                                                        title="Standard"
                                                     >
-                                                        <div className="w-100 px-3 text-center mb-2 NunitoSans-Bold">
-                                                            <Divider mb="4" />
-                                                        </div>
-                                                        <div className="d-flex mb-3 mt-1 text-center justify-content-center">
-                                                                <FormLabel ml="12px" htmlFor="license">
-                                                                    Physical
-                                                                </FormLabel>
-                                                                <Switch color="primary"
-                                                                    onChange={(e) => {
-                                                                    setSwitch1(e.target.checked);
-                                                                    }}
-                                                                    isChecked={switch1}
-                                                                    id="license"
-                                                                />
-                                                                <FormLabel ml="12px" htmlFor="license">
-                                                                    Virtual
-                                                                </FormLabel>
-                                                        </div>
+                                                        
                                                         <div className="d-flex btn align-items-center">
                                                             <PricingQuote
                                                                 button
                                                                 serviceName="SD-WAN As A Service"
-                                                                serviceDescription={"Fortinet, Single"+handleSwitch(switch1)}
+                                                                serviceDescription={"Fortinet, Standard"}
                                                             ></PricingQuote>
                                                         </div>
                                                     </PricingCard>
                                                 </div>
-                                                <div className="col-lg-5 my-3">
+                                                <div className="col-lg-4 my-3">
                                                     <PricingCard
-                                                        featureList={features}
-                                                        icon="/assets/images/icons/theme/globe.svg"
-                                                        title="Cluster"
+                                                        featureList={fortnietFeatures.slice(0,6)}
+                                                        excludingList={fortnietFeatures.slice(6,11)}
+                                                        icon="/assets/images/icons/theme/paper-plane.svg"
+                                                        title="Professional"
                                                     >
-                                                        <div className="w-100 px-3 text-center mb-2 NunitoSans-Bold">
-                                                            <Divider mb="4" />
-                                                        </div>
-                                                        <div className="d-flex mb-3 mt-1 text-center justify-content-center">
-                                                                <FormLabel ml="12px" htmlFor="license">
-                                                                    Physical
-                                                                </FormLabel>
-                                                                <Switch color="primary"
-                                                                    onChange={(e) => {
-                                                                    setSwitch2(e.target.checked);
-                                                                    }}
-                                                                    isChecked={switch2}
-                                                                    id="license"
-                                                                />
-                                                                <FormLabel ml="12px" htmlFor="license">
-                                                                    Virtual
-                                                                </FormLabel>
-                                                        </div>
+                                                        
                                                         <div className="d-flex btn align-items-center">
                                                             <PricingQuote
                                                                 button
                                                                 serviceName="SD-WAN As A Service"
-                                                                serviceDescription={"Fortinet, Cluster"+handleSwitch(switch2)}
+                                                                serviceDescription={"Fortinet, Professional"}
+                                                            ></PricingQuote>
+                                                        </div>
+                                                    </PricingCard>
+                                                </div>
+                                                <div className="col-lg-4 my-3">
+                                                    <PricingCard
+                                                        featureList={fortnietFeatures.slice(0,11)}
+                                                        icon="/assets/images/icons/theme/airplane.svg"
+                                                        title="Business"
+                                                    >
+                                                        
+                                                        <div className="d-flex btn align-items-center">
+                                                            <PricingQuote
+                                                                button
+                                                                serviceName="SD-WAN As A Service"
+                                                                serviceDescription={"Fortinet, Business"}
                                                             ></PricingQuote>
                                                         </div>
                                                     </PricingCard>
