ffe1809ddd4f6805e8edef43a5bf0d6b448b8b5d
ShubhBhardwaj11 <40259519+Shubh29nov@users.noreply.github.com>
2020-10-20 21:36:09 +0530

    commit

--- front-end/src/database/services.jsx
+++ front-end/src/database/services.jsx
@@ -201,7 +201,7 @@ const services = {
         tags: ["architectural", "consultancy", "architects", "technology", "stack", "future-proof", "planning", "engineering"],
         description: "Load balancing is the most scalable methodology for handling many requests from modern multi-application....",
         illustration: "",
-        relatedService: ["Backup As A Service", "Database As A Service", "DR As A Service"],
+        relatedService: ["Firewall As A Service", "CEX As A Service", "DNS As A Service"],
         conclusion: "Load Balancing is the solution you’ve been looking for.",
         sections: sections.loadBalancer,
         popular: true
